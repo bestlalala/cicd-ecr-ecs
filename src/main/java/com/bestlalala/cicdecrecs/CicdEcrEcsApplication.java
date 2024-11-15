b2fe30ed5cde69ffd956b50a9ba2401b055f4e51
yeonsu <bestdustn@sookmyung.ac.kr>
2024-11-15 15:57:43 +0900

    setting

---
+++ src/main/java/com/bestlalala/cicdecrecs/CicdEcrEcsApplication.java
@@ -0,0 +1,13 @@
+package com.bestlalala.cicdecrecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdEcrEcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdEcrEcsApplication.class, args);
+    }
+
+}
